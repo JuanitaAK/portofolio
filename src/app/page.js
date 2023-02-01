e070074967c735ae547bd7a89232dabedc07e79e
JuanitaAK <afanadorjuanita@gmail.com>
2023-02-01 16:52:26 +0100

    woring in Profil component. image and social network icons added. working in image styling at the moment

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,22 @@
 //import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import { BsFillMoonStarsFill} from "react-icons/bs"
+//import { BsFillMoonStarsFill} from "react-icons/bs"
+import NavBar from './components/NavBar'
+import Profile from './components/Profile'
 
-//import styles from './page.module.css'
 
 
-const inter = Inter({ subsets: ['latin'] })
+
+
+//const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className="bg-white px-10">
       <section className="min-h-screen">
-        <nav className="py-10 mb-12 flex justify-between">
-        <h1 className="text-xl font-burtons">Developed by JuanitaK</h1>
-        <ul className="flex items-center">
-          <li>
-            <BsFillMoonStarsFill className="cursor-pointer text-2xl"/> 
-           </li>
-          <li><a 
-            className="bg-gradient-to-r bg-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" 
-            href='#'
-            >
-              Resume 
-              </a>
-              </li>
-          </ul>  
-        </nav>
-        
+       <NavBar/>
+       <Profile/>      
+
       </section>   
     </main>
   )
