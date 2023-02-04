f2e0c5fd801690ccac26d25a638e78c4b17511ad
JuanitaAK <afanadorjuanita@gmail.com>
2023-02-04 04:16:36 +0100

    images and presentation of projects added

--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import pico  from '../../../public/projects/pico8.gif';
 import code  from '../../../public/code2.png';
+import weather from '../../../public/projects/weather.png';
+import onAir from '../../../public/projects/onAir.png';
+import ecom from '../../../public/projects/ecom2.png';
+import todo from '../../../public/projects/todo.png'
+import meme from '../../../public/projects/meme1.png';
+import surf from '../../../public/projects/surf.png'
 
 
 
@@ -12,7 +18,7 @@ export default function Projects() {
                 <h2 className="text-5xl py-2 text-teal-600 font-medium">
                     Creations </h2>
                 <div className="py-5 relative flex justify-center ">
-                    <Image src={code} width={100} height={100} />
+                    <Image src={code} alt=" icon " width={100} height={100} />
                 </div>
                 
                 <h3 className="text-2xl py-2"> 
@@ -22,11 +28,117 @@ export default function Projects() {
                 </p>
             </div>
 
-            <div className="lg:flex gap-15">
+            <div className="relative lg: grid lg:grid-cols-2 gap-15 mx-auto">
 
-            <div className="p-10 shadow-xl rounded-2xl my-5 mx-5 ">
-                    <h3 className="text-2xl py-2 text-center text-gray-800">Michonne's Zombalad </h3>
-                    <Image src={pico} className=" items-center py-5 relative p-5 mx-auto" />
+                
+
+                <div className="p-10 shadow-xl rounded-2xl my-5 mx-5 ">
+                    <h3 className="text-2xl py-2 text-center text-gray-800"> On Air Today</h3>
+                    <Image src={onAir} alt="todo list screen capture" className=" items-center py-5 relative p-5 mx-auto" />
+                    <ul className=" items-center grid gap-5 grid-cols-1 grid-rows-1">
+                        
+                        <div className="flex-col items-center p-3">
+                            <p className="text-sm text-center pb-5 leading-6 text-gray-800">
+                            Chrome extension featuring the television schedule of the day in the United States. Group project. </p>
+                            <div className="flex justify-between gap-4"> 
+                                <a
+                                    className="text-center gap-2 m-0 bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md mx-auto"
+                                    href="https://github.com/JuanitaAK/On-air-Today">
+                                    Repository
+                                </a>
+                                
+                            </div>
+                        </div>
+                    </ul>
+                </div> 
+                
+
+                <div className="p-10 shadow-xl rounded-2xl my-5 mx-5 ">
+                    <h3 className="text-2xl py-2 text-center text-gray-800">Surf Spots</h3>
+                    <Image src={surf} alt="screen capture of surf spot app" className=" items-center py-5 relative p-5 mx-auto" />
+                    <ul className=" items-center grid gap-5 grid-cols-1 grid-rows-1">
+                        
+                        <div className="flex-col items-center p-3">
+                            <p className="text-sm text-center pb-5 leading-6 text-gray-800">
+                            iOS App. OOP programming in Swift and Python using Flask and Xcode. API call-creation. </p>
+                            <div className="flex justify-between gap-4"> 
+                                <a
+                                    className="text-center gap-2 m-0 bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md mx-auto"
+                                    href="https://github.com/JuanitaAK/SurfApp">
+                                    Repository
+                                </a>
+                                
+                            </div>
+                        </div>
+                    </ul>
+                </div> 
+
+                <div className="p-10 shadow-xl rounded-2xl my-5 mx-5 ">
+                    <h3 className="text-2xl py-2 text-center text-gray-800">Old fourniture for a new life.</h3>
+                    <Image src={ecom} alt="image of e-commerce website"  className=" items-center py-5 relative p-5 mx-auto" />
+                    <ul className=" items-center grid gap-5 grid-cols-1 grid-rows-1">
+                        
+                        <div className="flex-col items-center p-3">
+                            <p className="text-sm text-center pb-5 leading-6 text-gray-800">
+                            Back-end and Front-End E-Commerce website. API creation. Group project in JavaScript with React-Bootstrap, Node.js, Express, MongoDB.  </p>
+                            <div className="flex justify-between gap-4"> 
+                                <a
+                                    className="text-center gap-2 m-0 bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md mx-auto"
+                                    href="https://github.com/JuanitaAK/projet-collectif---back-fraises">
+                                    Back-End
+                                </a>
+                                <a
+                                    className="text-center gap-2 m-0 bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md mx-auto"
+                                    href="https://github.com/JuanitaAK/projet-collectif---front-fraises1">
+                                    Front-End
+                                </a>
+                            </div>
+                        </div>
+                    </ul>
+                </div> 
+                
+                <div className="p-10 shadow-xl rounded-2xl my-5 mx-5 ">
+                    <h3 className="text-2xl py-2 text-center text-gray-800">One day todo.</h3>
+                    <Image src={todo} alt="screen capture of a todo" className=" items-center py-5 relative p-5 mx-auto" />
+                    <ul className=" items-center grid gap-5 grid-cols-1 grid-rows-1">
+                        <div className="flex-col items-center p-3">
+                            <p className="text-sm text-center pb-5 leading-6 text-gray-800">
+                            Online Todo list. Personal project. Programming in JavaScript and CSS with React. Creation and use of props, states and keys, to pass data from user from different components. </p>
+                            <div className="flex justify-between gap-4"> 
+                                <a
+                                    className="text-center gap-2 m-0 bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md mx-auto"
+                                    href="https://github.com/JuanitaAK/One_day_todo">
+                                    Repository
+                                </a>
+                                
+                            </div>
+                        </div>
+                    </ul>
+                </div> 
+
+                <div className="p-10 shadow-xl rounded-2xl my-5 mx-5 ">
+                    <h3 className="text-2xl py-2 text-center text-gray-800">Meme World</h3>
+                    <Image src={meme} alt="screen capture of a browser extension " className=" items-center py-5 relative p-5 mx-auto" />
+                    <ul className=" items-center grid gap-5 grid-cols-1 grid-rows-1">
+                        
+                        <div className="flex-col items-center p-3">
+                            <p className="text-sm text-center pb-5 leading-6 text-gray-800">
+                            Back-end and Front-End microblogging platform. OOP programming, MVC pattern in Php and MySQL. Laravel framework. Use of Git, GitHub and Docker. Pair programming. </p>
+                            <div className="flex justify-between gap-4"> 
+                                <a
+                                    className="text-center gap-2 m-0 bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md mx-auto"
+                                    href="https://github.com/adatechschool/JAS-Plateforme-de-microblogging">
+                                    Repository
+                                </a>
+                                
+                            </div>
+                        </div>
+                    </ul>
+                </div> 
+
+                 <div className="p-10 shadow-xl rounded-2xl my-5 mx-5 ">
+                    <h3 className="text-2xl py-2 text-center text-gray-800">Michonne's Zombalad</h3>
+                    <Image src={pico} alt="screen capture of a video-game" className=" items-center py-5 relative p-5 mx-auto" />
                     <ul className=" items-center grid gap-5 grid-cols-1 grid-rows-1">
                         
                         <div className="flex-col items-center p-3">
@@ -45,23 +157,22 @@ export default function Projects() {
                                 </a>
                             </div>
                         </div>
-
                     </ul>
                 </div> 
  
 
                 <div className="p-10 shadow-xl rounded-2xl my-5 mx-5 ">
-                    <h3 className="text-2xl py-2 text-center text-gray-800">Michonne's Zombalad </h3>
-                    <Image src={pico} className=" items-center py-5 relative p-5 mx-auto" />
+                    <h3 className="text-2xl py-2 text-center text-gray-800">Weather Website </h3>
+                    <Image src={weather} alt="screen capture of a weather webpage"  className=" items-center py-5 relative p-5 mx-auto" />
                     <ul className=" items-center grid gap-5 grid-cols-1 grid-rows-1">
                         
                         <div className="flex-col items-center p-3">
                             <p className="text-sm text-center pb-5 leading-6 text-gray-800">
-                                In a post-apocalyptic world, the player must escape from a mob of zombies. Programming in LUA language on the Pico-8 software. </p>
+                            Weather forecast visualization website by geolocation or by entering the location name. Calling two API. Group project. Programming in JavaScript, CSS and Html.   </p>
                             <div className="flex justify-between gap-4"> 
                                 <a
                                     className="text-center gap-2 m-0 bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md mx-auto"
-                                    href="https://github.com/JuanitaAK/Pico-8--michonne-s-zombelad-">
+                                    href="https://github.com/JuanitaAK/Weather-website">
                                     Repository
                                 </a>
                                 
@@ -70,25 +181,7 @@ export default function Projects() {
                     </ul>
                 </div>  
 
-                <div className="p-10 shadow-xl rounded-2xl my-5 mx-5 ">
-                    <h3 className="text-2xl py-2 text-center text-gray-800">Michonne's Zombalad </h3>
-                    <Image src={pico} className=" items-center py-5 relative p-5 mx-auto" />
-                    <ul className=" items-center grid gap-5 grid-cols-1 grid-rows-1">
-                        
-                        <div className="flex-col items-center p-3">
-                            <p className="text-sm text-center pb-5 leading-6 text-gray-800">
-                                In a post-apocalyptic world, the player must escape from a mob of zombies. Programming in LUA language on the Pico-8 software. </p>
-                            <div className="flex justify-between gap-4"> 
-                                <a
-                                    className="text-center gap-2 m-0 bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md mx-auto"
-                                    href="https://github.com/JuanitaAK/Pico-8--michonne-s-zombelad-">
-                                    Repository
-                                </a>
-                                
-                            </div>
-                        </div>
-                    </ul>
-                </div> 
+               
 
                   
             </div>
--- src/app/components/Stack.js
+++ src/app/components/Stack.js
@@ -1,18 +1,18 @@
 import Image from "next/image";
-import css from "../../../public/stack-icons/css-b.svg"
-import git from "../../../public/stack-icons/git-b.svg"
-import github from "../../../public/stack-icons/github-b.svg"
-import html from "../../../public/stack-icons/html-b.svg"
-import javascript from "../../../public/stack-icons/javascript-b.svg"
-import mongodb from "../../../public/stack-icons/mongodb-b.svg"
-import mysql from "../../../public/stack-icons/mysql-b.svg"
-import nextjs from "../../../public/stack-icons/nextjs-b.svg"
-import nodejs from "../../../public/stack-icons/nodejs-b.svg"
-import npm from "../../../public/stack-icons/npm-b.svg"
-import php from "../../../public/stack-icons/php-b.svg"
-import python from "../../../public/stack-icons/python-b.svg"
-import react from "../../../public/stack-icons/react-b.svg"
-import tailwind from "../../../public/stack-icons/tailwind-b.svg"
+import css from "../../../public/stack-icons/css-b.svg";
+import git from "../../../public/stack-icons/git-b.svg";
+import github from "../../../public/stack-icons/github-b.svg";
+import html from "../../../public/stack-icons/html-b.svg";
+import javascript from "../../../public/stack-icons/javascript-b.svg";
+import mongodb from "../../../public/stack-icons/mongodb-b.svg";
+import mysql from "../../../public/stack-icons/mysql-b.svg";
+import nextjs from "../../../public/stack-icons/nextjs-b.svg";
+import nodejs from "../../../public/stack-icons/nodejs-b.svg";
+import npm from "../../../public/stack-icons/npm-b.svg";
+import php from "../../../public/stack-icons/php-b.svg";
+import python from "../../../public/stack-icons/python-b.svg";
+import react from "../../../public/stack-icons/react-b.svg";
+import tailwind from "../../../public/stack-icons/tailwind-b.svg";
 
 
 export default function Stack() {
@@ -20,7 +20,7 @@ export default function Stack() {
     <section className="font-lato">
         <div className="text-center">
             <h3 className="text-4xl py-1">Tech Stack</h3>
-            {/* <Image src={css} width={50} height={50}/> ajouter image pour code  */}
+            {/* <Image alt="icon"src={css} width={50} height={50}/> ajouter image pour code  */}
             <p className="text-center py-2 leading-8 max-w-lg mx-auto m-center text-gray-800">During my studies at 
                 <span className="text-teal-500 px-1" >
                     <a href="https://adatechschool.fr/">
@@ -30,20 +30,20 @@ export default function Stack() {
         </div>
         <div className=" relative p-10 shadow-xl rounded-2xl my-5 mx-5">
             <ul className=" grid gap-10 grid-cols-5 grid-rows-3">
-                <Image src={css} width={50} height={50}/>
-                <Image src={tailwind} width={50} height={50}/>
-                <Image src={html} width={50} height={50}/>
-                <Image src={npm} width={50} height={50}/>
-                <Image src={javascript} width={50} height={50}/>
-                <Image src={nextjs} width={50} height={50}/>
-                <Image src={nodejs} width={50} height={50}/>
-                <Image src={react} width={50} height={50}/>
-                <Image src={php} width={50} height={50}/>
-                <Image src={python} width={50} height={50}/>
-                <Image src={mongodb} width={50} height={50}/>
-                <Image src={mysql} width={50} height={50}/>
-                <Image src={git} width={50} height={50}/>
-                <Image src={github} width={50} height={50}/>
+                <Image alt="icon"src={css} width={50} height={50}/>
+                <Image alt="icon"src={tailwind} width={50} height={50}/>
+                <Image alt="icon"src={html} width={50} height={50}/>
+                <Image alt="icon"src={npm} width={50} height={50}/>
+                <Image alt="icon"src={javascript} width={50} height={50}/>
+                <Image alt="icon"src={nextjs} width={50} height={50}/>
+                <Image alt="icon"src={nodejs} width={50} height={50}/>
+                <Image alt="icon"src={react} width={50} height={50}/>
+                <Image alt="icon"src={php} width={50} height={50}/>
+                <Image alt="icon"src={python} width={50} height={50}/>
+                <Image alt="icon"src={mongodb} width={50} height={50}/>
+                <Image alt="icon"src={mysql} width={50} height={50}/>
+                <Image alt="icon"src={git} width={50} height={50}/>
+                <Image alt="icon"src={github} width={50} height={50}/>
             </ul>
         </div>
     </section>
